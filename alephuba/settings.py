24d114591d19871503cbae563427885587b81429
Leandro Romero <leandro.s.romero@gmail.com>
2011-09-11 03:10:04 -0300

    Elimino los archivos .pyc

--- alephuba/settings.py
+++ alephuba/settings.py
@@ -14,8 +14,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'aleph',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'aleph.db',                      # Or path to database file if using sqlite3.
         'USER': 'admin',                      # Not used with sqlite3.
         'PASSWORD': 'admin',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
