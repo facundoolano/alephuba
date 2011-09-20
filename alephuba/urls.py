97a729073f6cdbef2b84809c8c0f619013dc4874
Facundo Olano <facundo.olano@gmail.com>
2011-09-20 18:47:52 -0300

    se movieron login y logout

--- alephuba/urls.py
+++ alephuba/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 from alephuba import settings
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, RedirectView
 from alephuba.aleph.views.generic_views import DocumentoList, DocumentoDetail,\
     DocumentoCreate
 from aleph.views import views, json_views
@@ -11,7 +11,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+    url(r'^$', django.contrib.auth.views.login, {'template_name': 'index.html'}, name='home'),
     url(r'^admin/', include(admin.site.urls), name='admin'),
     
     url(r'^docs/$', DocumentoList.as_view(), name='documentos'),
@@ -19,8 +19,7 @@ urlpatterns = patterns('',
     url(r'^docs/add/$', DocumentoCreate.as_view(), name='add_documento'),
     
     url(r'^registracion/$', views.registration, name='registracion'),
-    url(r'^login/$', django.contrib.auth.views.login, {'template_name': 'login.html'} , name='login'),
-    url(r'^logout/$', django.contrib.auth.views.logout, {'template_name': 'index.html'}, name='logout'),
+    url(r'^logout/$', django.contrib.auth.views.logout, {'next_page' : '/'}, name='logout'),
     url(r'^mi_cuenta/$', TemplateView.as_view(template_name='mi_cuenta.html'), name='mi_cuenta'),
     url(r'^cambio_contrasenia/$', django.contrib.auth.views.password_change,
        {'template_name': 'cambio_contrasenia.html'}, name='cambio_contrasenia'),
