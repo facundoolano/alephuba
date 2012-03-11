42bfd7b9daa51f87e2437754da8de14d26276771
Leandro Romero <leandro.s.romero@gmail.com>
2012-03-11 16:21:34 -0300

    Mejoro el admin. Agrego filtrado de documentos por tipo. Permito que se realizen busquedas en el admin.

--- alephuba/aleph/admin.py
+++ alephuba/aleph/admin.py
@@ -4,14 +4,25 @@ from alephuba.aleph.models import Carrera, Materia, Documento, UserProfile, Vote
 
 class MateriaAdmin(admin.ModelAdmin):
     list_display = ('codigo', 'nombre')
+    search_fields = ['codigo', 'nombre']
 
 class ArchivoAdmin(admin.ModelAdmin):
-    list_display = ('documento', 'link')
+    list_display = ('documento', 'link', 'subido_por')
+    search_fields = ['subido_por__username', 'documento__titulo']
+
+class DocumentoAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'autor', 'tipo']
+    search_fields = ['titulo', 'autor']
+    list_filter = ['tipo']
+
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ['user', 'document', 'vote_value']
+    search_fields = ['user__username', 'document__titulo']
 
 admin.site.register(Carrera)
 admin.site.register(Materia, MateriaAdmin)
-admin.site.register(Documento)
+admin.site.register(Documento, DocumentoAdmin)
 admin.site.register(Archivo, ArchivoAdmin)
 admin.site.register(UserProfile)
-admin.site.register(Vote)
+admin.site.register(Vote, VoteAdmin)
 
